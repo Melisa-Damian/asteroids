95f656527bba64181e06c64009ad02842da521ef
Melisa-Damian <damian_melisa@yahoo.com>
2026-04-01 15:25:43 +0300

    created Player functionality

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from logger import log_state
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
 
 def main():
     pygame.init()
@@ -8,6 +9,16 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    
+    time = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
@@ -17,8 +28,13 @@ def main():
                 return
 
         screen.fill("black")
-        pygame.display.flip()
 
+        for thing in drawable:
+            thing.draw(screen)
+        updatable.update(dt)
+
+        pygame.display.flip()
+        dt = time.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
